8de9c4119a98cc51d8a5ce4c933edfced776ece7
Rintu <Rintu@VINDUJA>
2022-11-05 15:50:41 +0530

    Add project files.

---
+++ Test_Vinay_Prime_Number/PrimeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Vinay_Prime_Number
+{
+    public class PrimeNumber
+    {
+        int count=0;
+        public void FindPrimeNumber()
+        {
+            Console.WriteLine("Enter any Number");
+            int num=int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if(count == 2)            
+                Console.WriteLine("Given Number Is PrimeNumber");            
+            else            
+                Console.WriteLine("Given Number Not a PrimeNumber");            
+        }
+    }
+}
---
+++ Test_Vinay_Prime_Number/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test_Vinay_Prime_Number;
+
+Console.WriteLine("Hello, World!");
+
+PrimeNumber objPrime = new PrimeNumber();
+objPrime.FindPrimeNumber();
